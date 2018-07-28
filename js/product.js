f0b53cf001d05e21d97e2d45c630f42764386e82
meliha <melihatak2015@gmail.com>
2018-07-28 14:57:47 +0300

    make some changes in products.html page

---
+++ js/product.js
@@ -0,0 +1,35 @@
+﻿$('.Main-iso').isotope({
+itemSelector:'.isotope-item',
+layoutMode:'fitRows'
+
+});
+
+$('.nav ul li').click(function(){
+
+  $('.nav ul li').removeClass('active');
+  $(this).addClass('active');
+
+  var selector =$(this).attr('data-filter');
+  $('.Main-iso').isotope({
+
+    filter:selector
+  });
+
+return false;
+
+
+});
+
+$('.Main-iso').isotope({
+  getSortData:{
+    name:function($elem){
+      return $elem.find('.Name').text();
+    },
+    ASC:function($elem){
+      return $elem.find('.ASC').text();
+    },
+    DESC:function($elem){
+      return $elem.find('.DESC').text();
+    }
+  }
+});
