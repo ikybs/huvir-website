fb072136eb8dede0405c5b066b306807d52309db
meliha <melihatak2015@gmail.com>
2018-07-30 10:59:54 +0300

    Değişiklikler üzerinde duruldu

--- js/product.js
+++ js/product.js
@@ -1,35 +1,50 @@
-﻿$('.Main-iso').isotope({
-itemSelector:'.isotope-item',
-layoutMode:'fitRows'
-
-});
-
-$('.nav ul li').click(function(){
-
-  $('.nav ul li').removeClass('active');
-  $(this).addClass('active');
-
-  var selector =$(this).attr('data-filter');
-  $('.Main-iso').isotope({
-
-    filter:selector
-  });
-
-return false;
-
-
-});
+﻿
+  
 
 $('.Main-iso').isotope({
-  getSortData:{
-    name:function($elem){
-      return $elem.find('.Name').text();
-    },
-    ASC:function($elem){
-      return $elem.find('.ASC').text();
-    },
-    DESC:function($elem){
-      return $elem.find('.DESC').text();
-    }
-  }
-});
+  itemSelector:'.isotope-item',
+  layoutMode:'fitRows'
+  
+  });
+  
+  $('.nav ul li').click(function(){
+  
+    $('.nav ul li').removeClass('active');
+    $(this).addClass('active');
+  
+    var selector =$(this).attr('data-filter');
+    $('.Main-iso').isotope({
+  
+      filter:selector
+    });
+  
+  return false;
+  
+  
+  });
+
+
+  $(document).ready(function(){
+    var $grid = $('.grid').isotope({
+      getSortData: {
+        name: '.name', // text from querySelector
+         brand:'.brand',
+         
+       /* category: '[data-category]', // value of attribute*/
+        /*price: function( itemElem ) { // function
+          var price = $( itemElem ).find('.price').text();
+          return parseFloat( price.replace( /[\(\)]/g, '') );
+        }*/
+      },
+      
+    });
+   
+  
+  
+  
+    $('.selected').on( 'change', function() {
+      var sortByValue = this.options[this.selectedIndex].getAttribute('data-sort-by');
+      $grid.isotope({ sortBy: sortByValue });
+    });
+  });
+  
