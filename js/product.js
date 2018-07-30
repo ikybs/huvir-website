da235912aaff388008ba1ccba1386416d3cd9a89
meliha <melihatak2015@gmail.com>
2018-07-30 14:07:45 +0300

    quick search eklendi

--- js/product.js
+++ js/product.js
@@ -1,12 +1,11 @@
-﻿
-  
-
-$('.Main-iso').isotope({
+﻿$('.Main-iso').isotope({
   itemSelector:'.isotope-item',
-  layoutMode:'fitRows'
-  
+ layoutMode:'fitRows',
+
   });
-  
+
+  /*filtering area*/
+
   $('.nav ul li').click(function(){
   
     $('.nav ul li').removeClass('active');
@@ -20,14 +19,20 @@ $('.Main-iso').isotope({
   
   return false;
   
-  
   });
+/*end of filtering area*/
 
 
+/*sorting area*/
+var qsRegex;
   $(document).ready(function(){
     var $grid = $('.grid').isotope({
-
+      filter: function() {
+        return qsRegex ? $(this).text().match( qsRegex ) : true;
+      },
+     
       getSortData: {
+        sortBy:'*',
         name: '.name', // text from querySelector
          brand:'.brand',
          
@@ -35,25 +40,79 @@ $('.Main-iso').isotope({
           var price =$(elem).find('.price').text();
 
           return parseFloat(price.replace('$', '') );
-          
+    }
+   
         }
 
-       /* category: '[data-category]', // value of attribute*/
-        /*price: function( itemElem ) { // function
-          var price = $( itemElem ).find('.price').text();
-          return parseFloat( price.replace( /[\(\)]/g, '') );
-        }.replace('$', '') )
-    },*/
-  }
     });
  
-  
-  
+ 
     $('.selected').on( 'change', function() {
+
       var sortByValue = this.options[this.selectedIndex].getAttribute('data-sort-by');
       $grid.isotope({ sortBy: sortByValue });
     });
+
+
+  // use value of search field to filter
+
+  var $quicksearch = $('.quicksearch').keyup( debounce( function() {
+    qsRegex = new RegExp( $quicksearch.val(), 'gi' );
+    $grid.isotope();
+  }, 200 ) );
+  
+  // debounce so filtering doesn't happen every millisecond
+  function debounce( fn, threshold ) {
+    var timeout;
+    threshold = threshold || 100;
+    return function debounced() {
+      clearTimeout( timeout );
+      var args = arguments;
+      var _this = this;
+      function delayed() {
+        fn.apply( _this, args );
+      }
+      timeout = setTimeout( delayed, threshold );
+    };
+  }
+
     
   });
   
+ /*end of the sorting*/
+
+ /*quick search with filtering isotope */
  
+// quick search regex
+/*
+var qsRegex;
+
+// init Isotope
+var $grid = $('.grid').isotope({
+  itemSelector: '.isotope-item',
+  layoutMode: 'fitRows',
+  filter: function() {
+    return qsRegex ? $(this).text().match( qsRegex ) : true;
+  }
+});
+
+// use value of search field to filter
+var $quicksearch = $('.quicksearch').keyup( debounce( function() {
+  qsRegex = new RegExp( $quicksearch.val(), 'gi' );
+  $grid.isotope();
+}, 200 ) );
+
+// debounce so filtering doesn't happen every millisecond
+function debounce( fn, threshold ) {
+  var timeout;
+  threshold = threshold || 100;
+  return function debounced() {
+    clearTimeout( timeout );
+    var args = arguments;
+    var _this = this;
+    function delayed() {
+      fn.apply( _this, args );
+    }
+    timeout = setTimeout( delayed, threshold );
+  };
+}*/
