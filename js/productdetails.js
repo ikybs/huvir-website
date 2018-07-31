ab49cc82debfd4e9ede28ec33f503c404c2133d6
meliha <melihatak2015@gmail.com>
2018-07-31 13:35:47 +0300

    adding some data

--- js/productdetails.js
+++ js/productdetails.js
@@ -5,7 +5,7 @@
         navigation : false, // Show next and prev buttons
         slideSpeed : 300,
         paginationSpeed : 400,
-        singleItem:false
+        singleItem:true
    
         
    
